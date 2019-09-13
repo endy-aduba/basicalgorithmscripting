ad46da86285819899543bb45f49858c3571d7893
endy <endyaduba@gmail.com>
2019-09-13 16:16:38 +0100

    after a one week hiatus

---
+++ boohoo.js
@@ -0,0 +1,14 @@
+function booWho(bool) {
+    // What is the new fad diet for ghost developers? The Boolean.
+    if(bool === false){
+      return true;
+    }
+    else if(bool === true ){
+      return true;
+    }
+    else{
+      return false
+    }
+  }
+  
+  booWho(null);
---
+++ titlecaseasentence.js
@@ -0,0 +1,9 @@
+// I really dislike this one cus I spent so much more time than i was supposed to on it. Either way, I guess it's done now.
+function titleCase(str) {
+    let str1 = str.toLowerCase();
+    let words = str1.split(" ");
+    for(let i=0;i<words.length;i++)
+    return str;
+  }
+  
+console.log(  titleCase("I'm a little tea pot"));
